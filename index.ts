2cbffa579be4ce7a9a57bb7f5d388c01bbc80336
M.A.S <160266513+m-Alishahid@users.noreply.github.com>
2024-03-26 12:56:47 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+const randomNumber=Math.floor(Math.random() * 6+1);
+const question=await inquirer.prompt([{
+      name:"userInputGuess",
+      type:"number",
+      message:"Please Guess a number under 1-6:",
+}]);
+if(question.userInputGuess===randomNumber){
+console.log("Great! you guess a right number");
+
+}
+else{
+    console.log("You guess wrong number try again!");
+}
